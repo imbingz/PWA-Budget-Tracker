b0759964693a7cec001de082745cfac33af294be
Bing Z <bing@bings-mbp.lan>
2020-11-20 14:48:33 -0700

    added port  n connection env  var

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+//Import required packages
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+//Set up PORT env or localhost
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+//Initialize middelwares
 app.use(logger("dev"));
 
 app.use(compression());
@@ -15,14 +18,23 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-	useNewUrlParser: true,
-	useFindAndModify: false
-});
-
-// routes
+// routes handler
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-	console.log(`App running on port ${PORT}! https://localhost:${PORT}`);
-});
+//Connect to mongodb atlas and server
+mongoose
+	.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false
+	})
+	.then(() => {
+		//Setup Server after connecting to db
+		app.listen(PORT, () => {
+			console.log(`listening on PORT ${PORT}, http://localhost:${PORT}`);
+		});
+	})
+	.catch((error) => {
+		console.log(error.message);
+	});
