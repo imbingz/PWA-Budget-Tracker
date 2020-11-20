5c26d52de774087d4cf900c5baae94b23a230d21
Bing Z <bing@bings-mbp.lan>
2020-11-20 15:50:06 -0700

    added sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,47 +1,67 @@
-console.log("reached sw.js");
+console.log("hit SW file");
 
-const FILE_CACHE_NAME = "cachedFile-v1";
-const DATA_CACHE_NAME = "cachedData-v1";
+const fileCacheName = "file-v2";
+const dataCacheName = "data-v1";
 
-const urlsToCache = [
+const filesToCache = [
 	"/",
 	"/index.html",
 	"/style.css",
+	// "/db.js",
 	"/index.js",
-	"/db.js",
+	"/manifest.webmanifest",
 	"/icons/icon-192x192.png",
 	"/icons/icon-512x512.png"
 ];
 
-self.addEventListener("install", function(event) {
-	console.log("hit installation");
+// install lifecycle method
+self.addEventListener("install", (event) => {
+	console.log("hit install");
 
-	// Perform install steps
 	event.waitUntil(
-		//waitUntil install completes and then cache files
 		caches
-			.open(FILE_CACHE_NAME)
-			.then(function(cache) {
-				console.log("Opened cache");
-				return cache.addAll(urlsToCache);
+			.open(fileCacheName)
+			.then((cache) => {
+				return cache.addAll(filesToCache);
 			})
 			.catch((error) => console.log("error caching files on install: ", error))
 	);
-	//forces the waiting service worker to become the active service worker.
+
 	self.skipWaiting();
 });
 
-self.addEventListener("fetch", function(event) {
-	console.log("hit fetch");
-	console.log("fetch event:", event);
+self.addEventListener("activate", (event) => {
+	console.log("hit activation");
 
-	// handle api caching n using cached data in indexDB
+	event.waitUntil(
+		caches
+			.keys()
+			.then((keyList) => {
+				return Promise.all(
+					keyList.map((key) => {
+						// if current key does not equal current cache name, delete it
+						if (key !== fileCacheName && key !== dataCacheName) {
+							console.log("deleting cache: ", key);
+							return caches.delete(key);
+						}
+					})
+				);
+			})
+			.catch((error) => console.log("activation error: ", error))
+	);
+
+	// if any open clients, update to active SW
+	self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+	console.log(event);
+
+	// handle api caching
 	if (event.request.url.includes("/api")) {
-		console.log("hit fetch /api");
-
 		return event.respondWith(
 			caches
-				.open(DATA_CACHE_NAME)
+				.open(dataCacheName)
 				.then((cache) => {
 					return fetch(event.request)
 						.then((response) => {
@@ -59,33 +79,27 @@ self.addEventListener("fetch", function(event) {
 				.catch((error) => console.log("error fetching api: ", error))
 		);
 	}
+
 	event.respondWith(
 		caches
 			.match(event.request)
-			.then(function(response) {
-				// Cache hit - return response
-				console.log("hit caches.match inside fetch!");
-
+			.then((response) => {
 				if (response) {
 					return response;
 				}
 
-				return fetch(event.request).then(function(response) {
-					// Check if we received a valid response
-					if (!response || response.status !== 200 || response.type !== "basic") {
+				return fetch(event.request).then((response) => {
+					if (!response || !response.basic || !response.status !== 200) {
 						console.log("fetch response: ", response);
 						return response;
 					}
 
-					// IMPORTANT: Clone the response. A response is a stream
-					// and because we want the browser to consume the response
-					// as well as the cache consuming the response, we need
-					// to clone it so we have two streams.
+					// response is a stream, reading will consume the response
 					const responseToCache = response.clone();
 
 					caches
-						.open(FILE_CACHE_NAME)
-						.then(function(cache) {
+						.open(cacheName)
+						.then((cache) => {
 							cache.put(event.request, responseToCache);
 						})
 						.catch((error) => console.log(error));
@@ -95,29 +109,4 @@ self.addEventListener("fetch", function(event) {
 			})
 			.catch((error) => console.log("error"))
 	);
-});
-
-self.addEventListener("activate", function(event) {
-	console.log("hit activate");
-
-	const cacheAllowlist = [ "pages-cache-v1", "blog-posts-cache-v1" ];
-
-	event.waitUntil(
-		//The keys() method of the Cache interface returns a Promise that resolves to an array of Cache keys.
-		caches
-			.keys()
-			.then(function(cacheNames) {
-				console.log(cacheNames);
-				return Promise.all(
-					cacheNames.map(function(cacheName) {
-						if (cacheAllowlist.indexOf(cacheName) === -1) {
-							return caches.delete(cacheName);
-						}
-					})
-				);
-			})
-			.catch((error) => console.log("activation error: ", error))
-	);
-	// if any open clients, update to active SW
-	self.clients.claim();
 });
